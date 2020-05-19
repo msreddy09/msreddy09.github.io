2cdca082df9cf4610c7c4ee264e42dedfb702db3
ms_dev <msudhakarreddy09@gmail.com>
2020-05-19 11:29:19 +0530

    updated UI
    Resume.2.0

---
+++ resume/skills.js
@@ -0,0 +1,39 @@
+window.mySkills = {
+    "skills": [
+        { "name": "HTML/HTML5", "per": "95%"},
+        { "name": "Javascript", "per": "85%"},
+        { "name": "jQuery", "per": "85%"},
+        { "name": "CSS/CSS3", "per": "80%"},
+        { "name": "Angular js", "per": "85%"},
+        { "name": "Angular", "per": "75%"},
+        { "name": "React", "per": "75%"},
+        { "name": "React Native", "per": "65%"},
+        { "name": "SAPUI5", "per": "60%"},
+        { "name": "NodeJs", "per": "70%"},
+        { "name": "Bacbkone", "per": "60%"},
+        { "name": "NetSuite", "per": "80%"},
+        { "name": "Jasmine", "per": "90%"},
+        { "name": "Mocha", "per": "80%"},
+        { "name": "Jest", "per": "80%"},
+        { "name": "Git", "per": "90%"},
+        { "name": "Perforce", "per": "90%"},
+        { "name": "Python", "per": "40%"},
+        { "name": "Java", "per": "45%"},
+        { "name": "YUI3", "per": "65%"},
+        { "name": "OOJS/ES6", "per": "80%"},
+        { "name": "Responsive Web design", "per": "75%"},
+        { "name": "SASS/LESS", "per": "75%"},
+    ],
+    "companies_template_obj":[
+        {"c_name": "Oracle", "DOJ": "May 2018 - Till Date", "exp": "Present", "logo":"https://media-exp1.licdn.com/dms/image/C4E0BAQGmS4ZS9i2T6w/company-logo_100_100/0?e=1597276800&v=beta&t=6olabTPefycLg9ZwC-HXVwy-JfJ9swqIkr9EBp79_yA", "current_role": "Sr. Platform Software Engineer II", "past_roles": "", "location": "Hyderabad, India", "other_loacations": ""},
+        {"c_name": "Wells Fargo", "DOJ": "Jan 2015 – Apr 2018", "exp": "3yr 4m", "logo":"https://media-exp1.licdn.com/dms/image/C560BAQHeDOdNfmP_dg/company-logo_100_100/0?e=1597276800&v=beta&t=4WeKUwkJ4KTH7cZjFhPGvqMPB0AbxJDNsuWXiEPxBm4", "current_role": "Technology Specialist", "past_roles": "", "location": "Hyderabad, India", "other_loacations": ""},
+        {"c_name": "Prokarma Softech", "DOJ": "Apr 2012 – Jan 2015", "exp": "2yr 10m", "logo":"https://media-exp1.licdn.com/dms/image/C510BAQHD9RRFi0B1Jg/company-logo_100_100/0?e=1597276800&v=beta&t=rOhHFyrc79IbQqekZ5ywgBswttXTr0ovuaqqgvePmH4", "current_role": "Sr. Software Engineer", "past_roles": "", "location": "Hyderabad, India", "other_loacations": ""},
+        {"c_name": "FireFlys Technologies", "DOJ": "June 2010 - March 2012", "exp": "1yr 10m", "logo":"", "current_role": "Software Engineer", "past_roles": "", "location": "Hyderabad, India", "other_loacations": ""}
+    ],
+    "education": [
+        {"course": "Master of Computer Apllications", "year": "2010", "institute": "Hindustan University", "per": "7.8", "location": "Chennai, Tamilanadu"},
+        {"course": "B.S.C computers", "year": "2007", "institute": "Sriji College", "per": "7.8", "location": "Andhrarpradesh"},
+        {"course": "Intermediate", "year": "2004", "institute": "MSR Jr College", "per": "8.4", "location": "Andhrarpradesh"},
+        {"course": "SSC", "year": "2002", "institute": "Saraswathi High school", "per": "7.8", "location": "Andhrarpradesh"}
+    ]
+}
---
+++ script.js
@@ -0,0 +1,132 @@
+var ms_dev = {};
+(context => { 
+    context.companies_template_obj = mySkills.companies_template_obj;
+    context.education_template_obj = mySkills.education;
+    context.createCompanies = () => {
+        $('body .companies_section').empty();  
+        context.companies_template_obj.forEach(company => {
+            context.companies_template = `
+            <div class='col-sm-12 col-md-4'>
+                <div class="card">
+                    <img class ='logo' src=${company.logo}>
+                    <div class="card-header">${company.c_name} <br><small style='color: gray; font-size: 70%'>${company.exp}</small> </div>
+                    <div class="card-body">
+                        <small>
+                            <div class='card-text'> ${company.current_role}</div>
+                            <div>${company.DOJ}</div>
+                            <div>${company.location}</div>
+                        </small>
+                    </div>
+                </div>
+            </div>`;
+            $('body .companies_section').append(context.companies_template);
+        });
+    }
+    context.createSkills = () => {
+        $('#skills #prof div').remove();
+        $('#skills #interm div').remove();
+        $('#skills #begginer div').remove();
+        mySkills.skills.forEach(skill => {
+            let skillTemplate;
+            let progresStyle = 'bg-success';
+            let elCont = ''
+            let perInt = parseInt(skill.per)
+            if(perInt >= 70) {
+                progresStyle = 'bg-success';
+                elCont = '#prof'
+            }else if(perInt > 50 && perInt < 70) {
+                progresStyle = 'bg-info';
+                elCont = '#interm'
+            }else{
+                progresStyle = 'bg-warning';
+                elCont = '#begginer'
+            }
+            skillTemplate =  `<div class="progress">
+            <div class="progress-bar ${progresStyle}" role="progressbar" style="width: ${skill.per}" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+            <div style="
+            line-height: 10;
+            line-height: 15px;
+            position: absolute;
+            right: 5px; color: #ccc; font-weight: bold;"
+            
+            >${skill.per}</div>
+            <div style="
+            line-height: 10;
+            line-height: 15px;
+            position: absolute;
+            left: 10px;">${skill.name}</div>
+            </div>
+            </div>`
+            //$('#skills '+ elCont).empty();
+            $('#skills '+ elCont).append(skillTemplate);
+        })
+    }
+
+    context.createEducation = () => {
+        $('body #edu .education_section').empty();
+        context.education_template_obj.forEach(edu => {
+            context.education_template = `
+            <div class='col-sm-12 col-md-4'>
+                <div class="card">
+                    <div class="card-header">${edu.course} <br><small style='color: gray; font-size: 70%'><b>Grade: </b>${edu.per}</small> </div>
+                    <div class="card-body">
+                        <small>
+                            <div class='card-text'> ${edu.institute}</div>
+                            <div>${edu.year}</div>
+                            <div>${edu.location}</div>
+                        </small>
+                    </div>
+                </div>
+            </div>`;
+            $('body #edu .education_section').append(context.education_template);
+        });
+
+    }
+
+    context.createContat = () => {
+        $('#contact').empty();
+        let contact_template = `<div class="media">
+        <img width="64" style='margin-right: 15px;border-radius: 32px' src="https://media-exp1.licdn.com/dms/image/C5103AQGRGig68EGT1Q/profile-displayphoto-shrink_100_100/0?e=1594857600&amp;v=beta&amp;t=6Pgfwsw2L5PbM89MRSgDD20u4oz7x-fBzuKGCVD-PPI" loading="lazy" height="64" alt="" id="ember799" class="d-none d-sm-none d-md-block feed-identity-module__member-photo profile-rail-card__member-photo EntityPhoto-circle-5 lazy-image ember-view">
+        <div class="media-body">
+          <h5 class="mt-0">Sudhakar Reddy Medagam</h5>
+          <div>Phone: +91 950203078</div>
+          <div>Email: msudhakarreddy09@gmail.com</div>
+          <div><a href="httdivs://www.linkedin.com/in/msreddy09/">https://www.linkedin.com/in/msreddy09</a> </div>
+          <div><a href="httdivs://github.com/msreddy09">GitHub</a> </div>            
+          <div><a class='btn btn-primary'
+            href='https://docs.google.com/gview?url=https://raw.githubusercontent.com/msreddy09/msreddy09.github.io/master/MSReddy_Resume.pdf'
+            download>Download Resume</a> </div>
+        </div>
+      </div>`
+      $('#contact').append(contact_template);
+    }
+    //context.createSkills();
+    //context.createCompanies();
+    //context.createEducation();
+    let tabs = document.querySelectorAll('.nav-link');
+    tabs.forEach((tab) => {
+        tab.addEventListener('click', function(event){
+            let tabName = (event.currentTarget.id);
+            switch(tabName) {
+                case 'skills-tab':  
+                    context.createSkills();
+                    break;
+                case 'exp-tab':
+                    context.createCompanies();
+                    break;
+                case 'port-tab':
+                    context.createEducation()
+                    break;
+                case 'edu-tab':  
+                    context.createEducation()
+                    break;
+                case 'contact-tab':  
+                    context.createContat();
+                    break;
+                default :  break;
+            }
+        })
+    })
+   
+})(ms_dev)
+
